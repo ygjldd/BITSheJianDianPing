0db5576c39386b43eb77ddc4819a6cd0976fedbb
Xiyuche <81409168+Xiyuche@users.noreply.github.com>
2023-06-04 18:25:12 +0800

    Add files via upload

---
+++ SheJianDianPing-Web/mock/asyncRoutes.ts
@@ -0,0 +1,40 @@
+// 模拟后端动态生成路由
+import { MockMethod } from "vite-plugin-mock";
+
+/**
+ * roles：页面级别权限，这里模拟二种 "admin"、"common"
+ * admin：管理员角色
+ * common：普通角色
+ */
+
+const permissionRouter = {
+  path: "/permission",
+  meta: {
+    title: "权限管理",
+    icon: "lollipop",
+    rank: 10
+  },
+  children: [
+    {
+      path: "/permission/page/index",
+      name: "PermissionPage",
+      meta: {
+        title: "管理员账户管理",
+        roles: ["admin", "common"]
+      }
+    }
+  ]
+};
+
+export default [
+  {
+    url: "/getAsyncRoutes",
+    method: "get",
+    response: () => {
+      return {
+        success: true,
+        data: [permissionRouter]
+      };
+    }
+  }
+] as MockMethod[];
---
+++ SheJianDianPing-Web/vue.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   devServer: {
+//     proxy: {
+//       "/root": {
+//         target: "http://47.94.160.129:8080",
+//         changeOrigin: true,
+//         pathRewrite: {
+//           "^/root": ""
+//         }
+//       }
+//     }
+//   }
+// };
